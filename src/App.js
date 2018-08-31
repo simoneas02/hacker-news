a746c1c232ad1209f7dfd1b64178827974aa2724
simoneas02 <simoneas02@hotmail.com>
2018-08-31 13:51:52 -0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
+import Button from "./Button";
 import List from "./List";
 import Search from "./Search";
 
 const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = "100";
+
 const PATH_BASE = "https://hn.algolia.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage=";
 
 class App extends Component {
   constructor(props) {
@@ -14,7 +19,8 @@ class App extends Component {
     this.state = {
       title: "My Title",
       result: null,
-      searchTerm: DEFAULT_QUERY
+      searchTerm: DEFAULT_QUERY,
+      searchKey: ""
     };
 
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
@@ -24,8 +30,10 @@ class App extends Component {
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
@@ -33,22 +41,28 @@ class App extends Component {
 
   componentDidMount() {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchSubmit(event) {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
     event.preventDefault();
   }
 
   onSearchChange(event) {
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchTerm: event.target.value });
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
 
   onDimiss(id) {
@@ -59,6 +73,7 @@ class App extends Component {
 
   render() {
     const { title, searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
 
     if (!result) {
       return null;
@@ -77,6 +92,14 @@ class App extends Component {
         </Search>
 
         {result && <List list={result.hits} onDimiss={this.onDimiss} />}
+
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
+          >
+            More
+          </Button>
+        </div>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,29 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import Button from './Button';
+import Button from "./Button";
 
 const List = ({ list, onDimiss }) => {
+  if (!list) return null;
 
-    if (!list) return null;
-
-    if (!list.length) {
-        return <p>Sorry, the list is empty.</p>;
-    
-    } else {
-        return (
-            list.map( item =>
-                <div key={ item.objectID }>
-                    <h2><a href={ item.url } target='_blank'>{ item.title }</a></h2>
-                    <p>{ item.author }</p>
-                    <p>{ item.comments }</p>
-                    <span>{ item.points }</span>
-                    <Button onClick={ () => onDimiss( item.objectID ) } >
-                        Dimiss
-                    </Button>
-                </div>  
-            )
-        )
-    }
-}
+  if (!list.length) {
+    return <p>Sorry, the list is empty.</p>;
+  } else {
+    return list.map(item => (
+      <div key={item.objectID}>
+        <h2>
+          <a href={item.url} target="_blank">
+            {item.title}
+          </a>
+        </h2>
+        <p>{item.author}</p>
+        <p>{item.comments}</p>
+        <span>{item.points}</span>
+        <Button onClick={() => onDimiss(item.objectID)}>Dimiss</Button>
+      </div>
+    ));
+  }
+};
 
 export default List;
