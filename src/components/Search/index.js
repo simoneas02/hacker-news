81d42c95c25817d9681c5d6597a95dfaa8ca9e65
simoneas02 <simoneas02@hotmail.com>
2018-09-03 13:17:34 -0300

    add focus to input search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const Search = ({ value, onChange, onSubmit, children }) => (
-  <form onSubmit={onSubmit}>
-    {children}
-    <input type="text" onChange={onChange} value={value} />
-    <button type="submit">{children}</button>
-  </form>
-);
+class Search extends Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const { value, onChange, onSubmit, children } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        {children}
+        <input
+          type="text"
+          onChange={onChange}
+          value={value}
+          ref={node => (this.input = node)}
+        />
+        <button type="submit">{children}</button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
