4e1f1a6d5fdd3b0bd453f007bcebd4730ad22825
simoneas02 <simoneas03@gmail.com>
2018-09-09 11:33:54 -0300

    add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,16 @@
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+// const req = require.context('../stories', true, /.stories.js$/);
+// function loadStories() {
+//   req.keys().forEach(filename => req(filename));
+// }
+
+// configure(loadStories, module);
+
+const req = require.context('../src/components', true, /stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ src/components/App/stories.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('App', module).add("I'm App", () => <h1>Hello App</h1>);
