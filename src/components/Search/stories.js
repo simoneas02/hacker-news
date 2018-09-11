fc20cc78bae3a9aebd9d8b13440c8d65ba4347a2
simoneas02 <simoneas03@gmail.com>
2018-09-10 22:51:57 -0300

    add addon to stories of the Search component

--- src/components/Search/stories.js
+++ src/components/Search/stories.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 import Search from '.';
 
-storiesOf('Search', module).add('Basic search', () => (
-  <Search value="redux" children="Search" />
-));
+storiesOf('Search', module).add('Basic search', () => {
+  const children = text('children', 'Search');
+  const value = text('text', 'redux');
+  const onSearchChange = action('search');
+  const onSearchSubmit = action('search');
+
+  return (
+    <Search
+      value={value}
+      children={children}
+      onChange={onSearchChange}
+      onSubmit={onSearchSubmit}
+    />
+  );
+});
