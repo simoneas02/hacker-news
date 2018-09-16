c3549995042057bdbb58d10083ae9cdf4061c117
simoneas02 <simoneas02@hotmail.com>
2018-09-16 17:07:45 -0300

    add teste for the Pagination component

--- src/components/Pagination/test.js
+++ src/components/Pagination/test.js
@@ -1,23 +1,161 @@
+'use strict';
 import React from 'react';
-import { expect } from 'chai';
-import Pagination from '.';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { expect as assert } from 'chai';
+import Pagination, { pagination } from '.';
 
 describe('Pagination', () => {
-  it('Pagination should be a function', () => {
-    expect(Pagination).to.be.a('function');
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Pagination />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
 
-  test('pagination({total: 1, acitivePage: 1}) should return [1]', () => {
-    const params = { total: 1, acitivePage: 1 };
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Pagination />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Pagination rules', () => {
+  test('pagination should be a function', () => {
+    assert(pagination).to.be.a('function');
+  });
+
+  test('pagination({ total: 1, activePage: 1 }) should return [1]', () => {
+    const params = { total: 1, activePage: 1 };
     const result = [1];
-
-    expect(Pagination(params)).to.be.deep.equal(result);
+    assert(pagination(params)).to.be.deep.equal(result);
   });
 
-  test('pagination({total: 2, acitivePage: 1}) should return [1, 2]', () => {
-    const params = { total: 2, acitivePage: 1 };
+  test('pagination({ total: 2, activePage: 1 }) should return [1, 2]', () => {
+    const params = { total: 2, activePage: 1 };
     const result = [1, 2];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
 
-    expect(Pagination(params)).to.be.deep.equal(result);
+  test('pagination({ total: 5, activePage: 1 }) should return [1, 2, 3, 4, 5]', () => {
+    const params = { total: 5, activePage: 1 };
+    const result = [1, 2, 3, 4, 5];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 6, activePage: 1 }) should return [1, 2, 3, "...", 6]', () => {
+    const params = { total: 6, activePage: 1 };
+    const result = [1, 2, 3, '...', 6];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 6, activePage: 2 }) should return [1, 2, 3, "...", 6]', () => {
+    const params = { total: 6, activePage: 2 };
+    const result = [1, 2, 3, '...', 6];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 6, activePage: 3 }) should return [1, 2, 3, 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 3 };
+    const result = [1, 2, 3, 4, 5, 6];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 6, activePage: 4 }) should return [1, 2, 3, 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 4 };
+    const result = [1, 2, 3, 4, 5, 6];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 6, activePage: 5 }) should return [1, "...", 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 5 };
+    const result = [1, '...', 4, 5, 6];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 6, activePage: 6 }) should return [1, "...", 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 6 };
+    const result = [1, '...', 4, 5, 6];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 7, activePage: 1 }) should return [1, 2, 3, "...", 7]', () => {
+    const params = { total: 7, activePage: 1 };
+    const result = [1, 2, 3, '...', 7];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 7, activePage: 3 }) should return [1, 2, 3, 4, "...", 7]', () => {
+    const params = { total: 7, activePage: 3 };
+    const result = [1, 2, 3, 4, '...', 7];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 7, activePage: 4 }) should return [1, 2, 3, 4, 5, 6, 7]', () => {
+    const params = { total: 7, activePage: 4 };
+    const result = [1, 2, 3, 4, 5, 6, 7];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 7, activePage: 5 }) should return [1, "...", 4, 5, 6, 7]', () => {
+    const params = { total: 7, activePage: 5 };
+    const result = [1, '...', 4, 5, 6, 7];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 7, activePage: 6 }) should return [1, "...", 5, 6, 7]', () => {
+    const params = { total: 7, activePage: 6 };
+    const result = [1, '...', 5, 6, 7];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 7, activePage: 7 }) should return [1, "...", 5, 6, 7]', () => {
+    const params = { total: 7, activePage: 7 };
+    const result = [1, '...', 5, 6, 7];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 15, activePage: 8 }) should return [1, "...", 7, 8, 9, "...", 15]', () => {
+    const params = { total: 15, activePage: 8 };
+    const result = [1, '...', 7, 8, 9, '...', 15];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: 15 }) should return [1, 2, 3, "...", 15]', () => {
+    const params = { total: 15 };
+    const result = [1, 2, 3, '...', 15];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination({}) should return [1]', () => {
+    const params = {};
+    const result = [1];
+    assert(pagination(params)).to.be.deep.equal(result);
+  });
+
+  test('pagination() should return [1]', () => {
+    const result = [1];
+    assert(pagination()).to.be.deep.equal(result);
+  });
+
+  test('pagination({ total: "abc", activePage: 1 }) should throw an error', () => {
+    const params = { total: 'abc', activePage: 1 };
+    const result = 'total should be a number';
+
+    try {
+      pagination(params);
+    } catch (e) {
+      assert(e.message).to.be.equal(result);
+    }
+  });
+
+  test('pagination({ total: 10, activePage: "1a" }) should throw an error', () => {
+    const params = { total: 10, activePage: '1a' };
+    const result = 'activePage should be a number';
+
+    try {
+      pagination(params);
+    } catch (e) {
+      assert(e.message).to.be.equal(result);
+    }
   });
 });
